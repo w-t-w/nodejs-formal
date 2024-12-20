fbe8300903e4990c81a8ed814452ad1236928686
w-t-w <15866369958@163.com>
2024-12-20 15:38:43 +0800

    feature: commonjs 模块基础部分回溯已顺利完成

--- module/commonjs/index.js
+++ module/commonjs/index.js
@@ -1,24 +1,26 @@
-console.log('require commonjs start!');
-require('./lib');
-console.log('require commonjs end!');
+// console.log('require commonjs start!');
+// require('./lib');
+// console.log('require commonjs end!');
+//
+// const character = require('./lib/character');
+//
+// console.log(character);
+// console.log(character.math(103, 106));
+//
+// character.dislike = {
+//     vegetable: 'ginger',
+//     fruit: 'durian'
+// };
+//
+// const timer = setTimeout(function () {
+//     console.log(character);
+//     character.car = 'Mercedes-Benz';
+//     clearTimeout(timer);
+// }, 500);
+//
+// const timer_ano = setTimeout(function () {
+//     console.log(character);
+//     clearTimeout(timer_ano);
+// }, 1500);
 
-const character = require('./lib/character');
-
-console.log(character);
-console.log(character.math(103, 106));
-
-character.dislike = {
-    vegetable: 'ginger',
-    fruit: 'durian'
-};
-
-const timer = setTimeout(function () {
-    console.log(character);
-    character.car = 'Mercedes-Benz';
-    clearTimeout(timer);
-}, 500);
-
-const timer_ano = setTimeout(function () {
-    console.log(character);
-    clearTimeout(timer_ano);
-}, 1500);
+console.log('commonjs 模块基础部分回溯已通过!如需查看使用,将上述代码的注释剔除即可!');
