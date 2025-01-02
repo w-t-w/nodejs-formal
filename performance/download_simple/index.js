54a80013d11ae65267b1c2ff9d0fc828f4a773d5
w-t-w <15866369958@163.com>
2025-01-02 16:36:53 +0800

    feature: nodejs 使用 apache bench 测试性能工具测试结果要点简易版部分回溯已通过

--- performance/download_simple/index.js
+++ performance/download_simple/index.js
@@ -1,19 +1,20 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
+// const http = require('http');
+// const fs = require('fs');
+// const path = require('path');
+//
+// const TEMPLATE_DIR = path.resolve(__dirname, './template/index.html');
+//
+// const PORT = 4000;
+//
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200);
+//     res.end(fs.readFileSync(TEMPLATE_DIR, 'utf-8'));
+// });
+//
+// server.listen(PORT, () => {
+//     console.log(`The server is running at http://localhost:${PORT}!`);
+// });
 
-const TEMPLATE_DIR = path.resolve(__dirname, './template/index.html');
-const STATIC_DIR = path.resolve(__dirname, './source/static');
-
-const PORT = 4000;
-
-const server = http.createServer((req, res) => {
-
-});
-
-server.listen(PORT, () => {
-    console.log(`The server is running at http://localhost:${PORT}!`);
-});
-
+console.log('nodejs 使用 apache bench 测试性能工具测试结果要点简易版部分回溯已通过!如需查看使用,将上述代码的注释剔除即可!');
 
 
--- performance/index.js
+++ performance/index.js
@@ -1,3 +1,4 @@
 // 使用 apache bench 测试性能工具测试结果要点
 require('./download');
+// 使用 apache bench 测试性能工具测试结果要点简易版
 require('./download_simple');
